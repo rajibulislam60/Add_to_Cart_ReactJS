a05ad6b0602b4d8d4faf23022fafb0fe70277fdd
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-12-22 13:46:35 +0600

    add js for add cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import Quantity from "./component/quantity";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  let data = {
+    title: "Google Pixel",
+    details: "a new generated phone. It's a good and awesome phone.",
+    price: "1099.69",
+    img: "./pixel.webp",
+  };
 
-export default App
+  let [show, setShow] = useState(false);
+  let [items, setItems] = useState([]);
+
+  const handleAddToCartBtn = () => {
+    setItems((prevItem) => [...prevItem, data]);
+    setShow(true);
+  };
+  const handleColseCart = () => {
+    setShow(false);
+  };
+  return (
+    <div className="container m-auto bg-slate-200 p-5">
+      <div className="flex gap-5 mb-3">
+        <img src={data.img} alt="phone" className="border border-black" />
+        <div>
+          <h2>{data.title}</h2>
+          <p>{data.details}</p>
+          <h3>${data.price}</h3>
+          <Quantity />
+          <button onClick={handleAddToCartBtn} className="border border-black">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+      {show && (
+        <div className="bg-black text-white p-5">
+          <h2>Your Cart</h2>
+          <button onClick={handleColseCart} className="border">
+            X
+          </button>
+          {items.map((item, index) => (
+            <p key={index}>{item.title}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/quantity.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const quantity = () => {
+  return (
+    <div>
+        <button>-</button>
+        <p>0</p>
+        <button>+</button>
+    </div>
+  )
+}
+
+export default quantity
